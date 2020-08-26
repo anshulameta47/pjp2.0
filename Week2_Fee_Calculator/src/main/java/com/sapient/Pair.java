88c591a159b112a428c112ce664327806fcd367e
anshulameta47 <anshulameta@gmail.com>
2020-08-26 23:24:14 +0530

    Fee Calculator added

---
+++ Week2_Fee_Calculator/src/main/java/com/sapient/Pair.java
@@ -0,0 +1,12 @@
+package com.sapient;
+
+public class Pair 
+{
+AttributesPojo Transaction;
+int Processing_Fee;
+public Pair(AttributesPojo transaction, int processing_Fee) {
+	super();
+	Transaction = transaction;
+	Processing_Fee = processing_Fee;
+}
+}
